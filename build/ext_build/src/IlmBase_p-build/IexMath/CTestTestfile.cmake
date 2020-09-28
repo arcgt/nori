e4e06f73cd9fc4eafd1757dbdc1c48c55698dde6
Tianhong Gan <tianhonggan@outlook.com>
2020-09-28 14:34:41 +0200

    add gitignore

--- build/ext_build/src/IlmBase_p-build/IexMath/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tian/Documents/University/Master/ComputerGraphics/nori/build/ext_build/src/IlmBase_p/IexMath
-# Build directory: /Users/tian/Documents/University/Master/ComputerGraphics/nori/build/ext_build/src/IlmBase_p-build/IexMath
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
--- build/ext_build/src/IlmBase_p-build/IlmThread/CTestTestfile.cmake
+++
@@ -1,6 +0,0 @@
-# CMake generated Testfile for 
-# Source directory: /Users/tian/Documents/University/Master/ComputerGraphics/nori/build/ext_build/src/IlmBase_p/IlmThread
-# Build directory: /Users/tian/Documents/University/Master/ComputerGraphics/nori/build/ext_build/src/IlmBase_p-build/IlmThread
-# 
-# This file includes the relevant testing commands required for 
-# testing this directory and lists subdirectories to be tested as well.
